d9fcbce1bed21f5d0e067bfb4ae0744383570425
Cody Cutrer <cody@instructure.com>
2015-07-27 00:02:33 -0600

    f

--- utah.c
+++ utah.c
@@ -316,7 +316,7 @@ int main(int argc, char **argv)
       //find_move(cube, size);
 
 
-      if (cube[2][0][1] == 'O')
+      if (cube[2][0][2] == 'X')
         sleep(3);
       if (!first_cube) {
         return 1;
