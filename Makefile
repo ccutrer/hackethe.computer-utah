31ae2b93f47b27e9bf2d6452ea046f57b700c412
Cody Cutrer <cody@instructure.com>
2015-07-26 23:34:52 -0600

    rewrite :)

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	true
+	gcc -o run -O0 -g utah.c
 
 .PHONY: all
--- utah.c
+++ utah.c
@@ -3,115 +3,278 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int find_utah(char ***cube, int size, int i1, int j1, char team, char my_team, char axis, int horizontal)
+int check_from_frd(char ***cube, int size, int x, int y, int z)
 {
-  int i, j, k, w, count, middle_count;
-  int *x, *y, *z;
-  if (axis == 'X') {
-    x = &i;
-    y = &j;
-    z = &k;
-  } else if (axis == 'Y') {
-    x = &j;
-    y = &k;
-    z = &i;
-  } else {
-    x = &k;
-    y = &i;
-    z = &j;
-  }
+  // there are 4 rotations * 2 mirrors * 3 axes = 24 to check
 
-  if (horizontal)
-  {
-    int *temp = x;
-    x = y;
-    y = temp;
-  }
+  // z held constant-------------------------------
+  // normal utah
+  if (x < size - 1 &&
+      y < size - 2 &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x + 1][y + 1][z] &&
+      cube[x][y][z] == cube[x][y + 2][z] ==
+      cube[x][y][z] == cube[x + 1][y + 2][z])
+    return 1;
+  // mirrored
+  if (x > 0 &&
+      y < size - 2 &&
+      cube[x][y][z] == cube[x - 1][y + 1][z] &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x - 1][y + 2][z] ==
+      cube[x][y][z] == cube[x][y + 2][z])
+    return 1;
+  // clockwise
+  if (x < size - 2 &&
+      y < size - 1 &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x + 1][y + 1][z] &&
+      cube[x][y][z] == cube[x + 2][y][z])
+    return 1;
+  // clockwise, flipped
+  if (x < size - 2 &&
+      y < size - 1 &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x + 1][y + 1][z] &&
+      cube[x][y][z] == cube[x + 2][y + 1][z])
+    return 1;
+  // half turn
+  if (x < size - 1 &&
+      y < size - 2 &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x + 1][y + 1][z] &&
+      cube[x][y][z] == cube[x + 1][y + 2][z])
+    return 1;
+  // half turn, flipped
+  if (x < size - 1 &&
+      y < size - 2 &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x + 1][y + 1][z] &&
+      cube[x][y][z] == cube[x][y + 2][z])
+    return 1;
+  // counter-clockwise
+  if (x > 0 &&
+      y < size - 1 &&
+      cube[x][y][z] == cube[x - 1][y + 1][z] &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x + 1][y + 1][z])
+    return 1;
+  // counter-clockwise, flipped
+  if (x < size - 2 &&
+      y < size - 1 &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x + 1][y + 1][z] &&
+      cube[x][y][z] == cube[x + 2][y][z] &&
+      cube[x][y][z] == cube[x + 2][y + 1][z])
+    return 1;
 
-  for (k = 0; k < size; ++k) {
-    count = 0;
-    middle_count = 0;
-    for (i = i1; i < i1 + 3; ++i) {
-      for (j = j1; j < j1 + 2; ++j) {
-        if (cube[*x][*y][*z] == team) {
-          ++count;
-          if (j - j1 == 1)
-           ++middle_count;
+  // y held constant-------------------------------
+  // normal utah
+  if (x < size - 1 &&
+      z < size - 2 &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x + 1][y][z + 1] &&
+      cube[x][y][z] == cube[x][y][z + 2] ==
+      cube[x][y][z] == cube[x + 1][y][z + 2])
+    return 1;
+  // mirrored
+  if (x > 0 &&
+      z < size - 2 &&
+      cube[x][y][z] == cube[x - 1][y][z + 1] &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x - 1][y][z + 2] ==
+      cube[x][y][z] == cube[x][y][z + 2])
+    return 1;
+  // clockwise
+  if (x < size - 2 &&
+      z < size - 1 &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x + 1][y][z + 1] &&
+      cube[x][y][z] == cube[x + 2][y][z])
+    return 1;
+  // clockwise, flipped
+  if (x < size - 2 &&
+      z < size - 1 &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x + 1][y][z + 1] &&
+      cube[x][y][z] == cube[x + 2][y][z + 1])
+    return 1;
+  // half turn
+  if (x < size - 1 &&
+      z < size - 2 &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x + 1][y][z + 1] &&
+      cube[x][y][z] == cube[x + 1][y][z + 2])
+    return 1;
+  // half turn, flipped
+  if (x < size - 1 &&
+      z < size - 2 &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x + 1][y][z + 1] &&
+      cube[x][y][z] == cube[x][y][z + 2])
+    return 1;
+  // counter-clockwise
+  if (x > 0 &&
+      z < size - 1 &&
+      cube[x][y][z] == cube[x - 1][y][z + 1] &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x + 1][y][z + 1])
+    return 1;
+  // counter-clockwise, flipped
+  if (x < size - 2 &&
+      z < size - 1 &&
+      cube[x][y][z] == cube[x + 1][y][z] &&
+      cube[x][y][z] == cube[x + 1][y][z + 1] &&
+      cube[x][y][z] == cube[x + 2][y][z] &&
+      cube[x][y][z] == cube[x + 2][y][z + 1])
+    return 1;
+
+  // x held constant-------------------------------
+  // normal utah
+  if (y < size - 1 &&
+      z < size - 2 &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x][y + 1][z + 1] &&
+      cube[x][y][z] == cube[x][y][z + 2] ==
+      cube[x][y][z] == cube[x][y + 1][z + 2])
+    return 1;
+  // mirrored
+  if (y > 0 &&
+      z < size - 2 &&
+      cube[x][y][z] == cube[x][y - 1][z + 1] &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x][y - 1][z + 2] ==
+      cube[x][y][z] == cube[x][y][z + 2])
+    return 1;
+  // clockwise
+  if (y < size - 2 &&
+      z < size - 1 &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x][y + 1][z + 1] &&
+      cube[x][y][z] == cube[x][y + 2][z])
+    return 1;
+  // clockwise, flipped
+  if (y < size - 2 &&
+      z < size - 1 &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x][y + 1][z + 1] &&
+      cube[x][y][z] == cube[x][y + 2][z + 1])
+    return 1;
+  // half turn
+  if (y < size - 1 &&
+      z < size - 2 &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x][y + 1][z + 1] &&
+      cube[x][y][z] == cube[x][y + 1][z + 2])
+    return 1;
+  // half turn, flipped
+  if (y < size - 1 &&
+      z < size - 2 &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x][y + 1][z + 1] &&
+      cube[x][y][z] == cube[x][y][z + 2])
+    return 1;
+  // counter-clockwise
+  if (y > 0 &&
+      z < size - 1 &&
+      cube[x][y][z] == cube[x][y - 1][z + 1] &&
+      cube[x][y][z] == cube[x][y][z + 1] &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x][y + 1][z + 1])
+    return 1;
+  // counter-clockwise, flipped
+  if (y < size - 2 &&
+      z < size - 1 &&
+      cube[x][y][z] == cube[x][y + 1][z] &&
+      cube[x][y][z] == cube[x][y + 1][z + 1] &&
+      cube[x][y][z] == cube[x][y + 2][z] &&
+      cube[x][y][z] == cube[x][y + 2][z + 1])
+    return 1;
+
+  return 0;
+}
+
+char check_from(char ***cube, int size, int x, int y, int z)
+{
+  // have to slide our check for two on either side
+  int i, j, k;
+  char found, best_found = 0;
+  cube[x][y][z] = 'X';
+  for (i = x - 2; i < x + 2 && i < size; ++i) {
+    if (i < 0)
+      continue;
+    for (j = y - 2; j < y + 2 && j < size; ++j) {
+      if (j < 0)
+        continue;
+      for (k = z - 2; k < z + 2 && k < size; ++k) {
+        if (k < 0)
+          continue;
+        if (cube[i][j][k] == '_')
+          continue;
+        cube[x][y][z] = 'X';
+        if (check_from_frd(cube, size, i, j, k))
+          return 'X';
+        cube[x][y][z] = 'O';
+        if (check_from_frd(cube, size, i, j, k))
+          best_found = 'O';
+      }
+    }
+  }
+  // restore the cube
+  cube[x][y][z] = '_';
+
+  return best_found;
+}
+
+int find_move(char ***cube, int size)
+{
+  int i, j, k;
+
+  int block_i = -1, block_j = -1, block_k = -1;
+  char move_for;
+
+  for (i = 0; i < size; ++i) {
+    for (j = 0; j < size; ++j) {
+      for (k = 0; k < size; ++k) {
+        if (cube[i][j][k] == '_') {
+          // if I move here, will it complete a Utah?
+          if ((move_for = check_from(cube, size, i, j, k))) {
+            if (move_for == 'X') {
+              return 1;
+            } else {
+              block_i = i;
+              block_j = j;
+              block_k = k;
+            }
+          }
         }
       }
     }
-
-    if (count == 5 || (count == 4 && middle_count != 0)) {
-      // find the available space
-      // check the middle first
-      j = j1 + 1;
-      i = i1;
-      if (cube[*x][*y][*z] == '_') {
-        cube[*x][*y][*z] = my_team;
-        return 1;
-      }
-      ++i;
-      if (cube[*x][*y][*z] == '_') {
-        cube[*x][*y][*z] = my_team;
-        return 1;
-      }
-      // now the corners
-      j = j1;
-      i = i1;
-      if (cube[*x][*y][*z] == '_') {
-        cube[*x][*y][*z] = my_team;
-        return 1;
-      }
-      ++i;
-      if (cube[*x][*y][*z] == '_') {
-        cube[*x][*y][*z] = my_team;
-        return 1;
-      }
-      j = j1 + 2;
-      i = i1;
-      if (cube[*x][*y][*z] == '_') {
-        cube[*x][*y][*z] = my_team;
-        return 1;
-      }
-      ++i;
-      if (cube[*x][*y][*z] == '_') {
-        cube[*x][*y][*z] = my_team;
-        return 1;
-      }
-    }
   }
 
-  return 0;
-}
-
-int find_move_axis(char ***cube, int size, char team, char my_team, char axis)
-{
-  int i, j;
-
-  for (i = 0; i <= size - 3; ++i) {
-    for (j = 0; j <= size - 2; ++j) {
-      if (find_utah(cube, size, i, j, team, my_team, axis, 1))
-        return 1;
-      if (find_utah(cube, size, j, i, team, my_team, axis, 0))
-        return 1;
-    }
+  // no win for X, but found a block for O
+  if (block_i != -1) {
+    cube[block_i][block_j][block_k] = 'X';
+    return 1;
   }
-
   return 0;
 }
 
-int find_move(char ***cube, int size, char team, char my_team)
-{
-  if (find_move_axis(cube, size, team, my_team, 'X'))
-    return 1;
-  if (find_move_axis(cube, size, team, my_team, 'Y'))
-    return 1;
-  if (find_move_axis(cube, size, team, my_team, 'Z'))
-    return 1;
-  return 0;
-}
-
-static const int MAX_SIZE = 1000;
+static const int MAX_SIZE = 10;
 
 int main(int argc, char **argv)
 {
@@ -150,13 +313,7 @@ int main(int argc, char **argv)
       // blank newline
       fgetc(file);
 
-      // If we can't find a winning move for X,
-      // find a winning move for O and block it
-      if (!find_move(cube, size, 'X', 'X')) {
-        if (!find_move(cube, size, 'O', 'X')) {
-          //sleep(2);
-        }
-      }
+      find_move(cube, size);
 
       if (!first_cube)
         putc('\n', stdout);
