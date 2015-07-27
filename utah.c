ad18fa0672866333b1f92edc48b3359bdabd6bc2
Cody Cutrer <cody@instructure.com>
2015-07-27 00:29:54 -0600

    f

--- utah.c
+++ utah.c
@@ -316,7 +316,7 @@ int main(int argc, char **argv)
       fgetc(file);
 
       if (!find_move(cube, size)) {
-        if (cube_count == 2)
+        if (cube_count == 3)
           sleep(3);
       }
 
